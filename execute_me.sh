4c04d08f66e166294e0c8e59269d2d486bfc6602
Vinni-Cedraz <Vinni.vcr@gmail.com>
2023-07-24 11:51:27 +0000

    using /home/vcedraz- instead of the too specific /nfs/homes/$USER

--- execute_me.sh
+++ execute_me.sh
@@ -5,5 +5,5 @@ docker build --build-arg DISPLAY=${DISPLAY} --build-arg XDG_RUNTIME_DIR=${XDG_RU
 docker run -it \
 	--mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
 	--mount type=bind,source=${XDG_RUNTIME_DIR},target=${XDG_RUNTIME_DIR} \
-	--mount type=bind,source=/nfs/homes/${USER}/.ssh/,target=/root/.ssh/ \
+	--mount type=bind,source=${HOME}/.ssh/,target=/root/.ssh/ \
 	--name my_ubuntu_container my_ubuntu_image
